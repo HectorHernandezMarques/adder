1721308fda227c359e67b58d132af157c8db9dc4
HectorHernandezMarques <H.Hernandez@alumnos.upm.es>
2019-11-07 15:40:21 +0100

    Change to functional approach. Disable shared library generation, so debug target doesn't install .lib file.

--- lib/Adder.cpp
+++ lib/Adder.cpp
@@ -1,5 +1,7 @@
 #include <Adder.h>
 
 int add(int sum1, int sum2) {
-    return sum1 + sum2;
+    return sum2 < 0 ? add(sum1 - 1, sum2 + 1)
+                    : sum2 > 0 ? add(sum1 + 1, sum2 - 1)
+                               : sum1;
 }
